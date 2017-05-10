f06377177417d9b849c5a0a4b6fc8b8a6bd62231
Cyberia 2045 <sagansociety86@gmail.com>
2017-05-10 18:40:23 -0400

    created a Ruby Elevator

--- elevator.rb
+++ elevator.rb
@@ -1,16 +1,26 @@
 class Elevator
 
-	def initialize
-		@floor = 1
+	attr_reader :floor
+
+	def initialize(floor)
+		cheery_greeting
+		@floor = floor
 	end
 
 	def go_up
-		@floor ++
-
+		@floor += 1 unless @floor >= 12
+		cheery_greeting
 	end
 
 	def go_down
-		@floor --
+		@floor -= 1 unless @floor <= 1
+		cheery_greeting
 	end
 
-end
+	def cheery_greeting
+		"Welcome to floor #{@floor}!  How may I help you?"
+	end
+
+	e = Elevator.new(1)
+
+end # ends the elevator class
